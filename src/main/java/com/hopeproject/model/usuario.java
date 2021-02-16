0f0032471554962001f5fdb8a55c7aa54919f44f
unknown <sml.gomess@gmail.com>
2021-02-16 14:07:42 -0300

    Project Started

---
+++ src/main/java/com/hopeproject/model/usuario.java
@@ -0,0 +1,86 @@
+package com.hopeproject.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
+public class usuario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String name;
+	
+	@Column(name = "nickname")
+	private String nickname;
+	
+	@Column(name = "senha")
+	private String password;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "classificacao")
+	private String classification;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getClassification() {
+		return classification;
+	}
+
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
+	
+    
+}
